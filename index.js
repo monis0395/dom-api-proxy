bb868ad4110bc285a1ff17e5901c03b93d3b1c02
kevin <kevin.josev@gmail.com>
2020-01-09 11:01:26 +0530

    Update index.js
    Adding proxy to dom tree

--- index.js
+++ index.js
@@ -11,8 +11,35 @@ function html2Dom(html) {
 }
 const html = fs.readFileSync('./sample.html');
 
+const rootElement = dom => {
+  if (Array.isArray(dom)) {
+    return {
+      name: 'root',
+      type: 'tag',
+      children: dom
+    };
+  }
+  return dom;
+};
+
+const getElement = node => {
+  if (!node) {
+    return;
+  }
+  if (node.type == 'tag') {
+    return new Proxy(node, {
+      get: function(target, key) {
+        return Reflect.get(target, key);
+      },
+      set: function(target, key, value) {
+        return Reflect.set(target, key, value);
+      }
+    });
+  }
+};
 
 async function main() {
   const dom = await html2Dom(html);
+  const document = getElement(rootElement(dom));
 }
 main();
